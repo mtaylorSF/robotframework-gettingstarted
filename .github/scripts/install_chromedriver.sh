27bed260ad8c766b7cf70f569f5e33acfc5aaf15
Taylor, Mike <mike.taylor@team.neustar>
2019-11-15 21:03:48 -0800

    trying out install scripts

---
+++ .github/scripts/install_chromedriver.sh
@@ -0,0 +1,6 @@
+## Chromedriver
+wget https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo chmod +x chromedriver
+sudo mv chromedriver /usr/bin/
+rm chromedriver_linux64.zip
